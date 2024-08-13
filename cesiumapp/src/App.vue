bd9217696035e62176575bd95cbf79b99615fb02
huangzhuo <idorable@163.com>
2024-08-14 02:28:42 +0800

    P2

---
+++ cesiumapp/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import * as Cesium from 'cesium';
+  import { onMounted } from 'vue';
+  onMounted(() => {
+    const viewer = new Cesium.Viewer('cesiumContainer');
+  })
+
+</script>
+
+<template>
+  <!-- <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" /> -->
+  <div id="cesiumContainer"></div>
+</template>
+
+<style scoped>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
+</style>
